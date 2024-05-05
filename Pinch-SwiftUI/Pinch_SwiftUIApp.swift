9e1b72dd8b21bc2b9cd6d7c5f26262765c5b4821
monir <mdmonirhelale@gmail.com>
2024-05-05 16:13:29 +0600

    Initial Commit

---
+++ Pinch-SwiftUI/Pinch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_SwiftUIApp.swift
+//  Pinch-SwiftUI
+//
+//  Created by Monir Haider Helalee on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
