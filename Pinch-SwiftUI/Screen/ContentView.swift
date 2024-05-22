3bbe73549f9dc0740626bc2474e80f7d368927e2
monir <mdmonirhelale@gmail.com>
2024-05-22 22:33:42 +0600

    added drawer

--- Pinch-SwiftUI/Screen/ContentView.swift
+++ Pinch-SwiftUI/Screen/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
 
     // MARK: - FUNCTION
 
@@ -69,17 +70,16 @@ struct ContentView: View {
                     // MARK: - Magnification
 
                     .gesture(MagnificationGesture()
-                        .onChanged{ value in
-                            withAnimation(.linear(duration: 1)){
-                                if imageScale >= 1 && imageScale <= 5 {
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 1)) {
+                                if imageScale >= 1, imageScale <= 5 {
                                     imageScale = value
-                                } else if imageScale > 5{
+                                } else if imageScale > 5 {
                                     imageScale = 5
                                 }
                             }
-                            
                         }
-                        .onEnded{ _ in
+                        .onEnded { _ in
                             if imageScale > 5 {
                                 imageScale = 5
                             } else if imageScale <= 1 {
@@ -151,6 +151,38 @@ struct ContentView: View {
             }
             .padding(.bottom, 30),
             alignment: .bottom)
+
+            // MARK: - Drawer
+
+            .overlay(
+                HStack(spacing: 12) {
+                    // MARK: - Drawer handle
+
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right": "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundColor(.secondary)
+                        .onTapGesture(perform: {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        })
+
+                    // MARK: - Thumbnail
+
+                    Spacer()
+                } //: Drawer
+                .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .opacity(isAnimating ? 1 : 0)
+                .frame(width: 260)
+                .padding(.top, UIScreen.main.bounds.height / 12)
+                .offset(x: isDrawerOpen ? 20 : 215),
+                alignment: .topTrailing
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
